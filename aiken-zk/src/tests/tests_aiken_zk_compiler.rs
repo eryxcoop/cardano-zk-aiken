371e51862a161637fc3a8434cbc88f4d30b0ca1c
bruno <bweisz@eryxsoluciones.com.ar>
2025-06-17 16:18:30 -0300

    Using tempdir instead of custom sandbox

--- aiken-zk/src/tests/tests_aiken_zk_compiler.rs
+++ aiken-zk/src/tests/tests_aiken_zk_compiler.rs
@@ -1,30 +1,31 @@
 use crate::aiken_zk_compiler::AikenZkCompiler;
 use std::fs;
 use std::env;
+use std::os::unix::fs::PermissionsExt;
+use std::path::Path;
+use tempfile::{tempdir, TempDir};
+use fs_extra::dir::{copy, CopyOptions};
+
 
 #[test]
 fn test_compiler_can_save_source_code_into_file(){
-    env::set_current_dir(&sandbox_path()).expect("Couldn't change directory");
+    let _temp_dir = create_sandbox_and_set_as_current_directory();
     let source_code = source_code_addition();
     let source_code_path = "test.circom";
 
     let mut compiler = AikenZkCompiler::from(source_code.clone());
-    let res = compiler.save_into_file(source_code_path.to_string());
+    compiler.save_into_file(source_code_path.to_string()).unwrap();
 
-    assert!(res.is_ok());
     let stored_content = fs::read_to_string(&source_code_path).expect("No se pudo leer el archivo");
     assert_eq!(source_code, stored_content);
     assert_eq!(source_code_path, compiler.circom_source_code_path.unwrap());
-
-    clear_sandbox()
 }
 
 #[test]
 fn test_compiler_can_compile_the_generated_circom_component(){
-    env::set_current_dir(&sandbox_path()).expect("Couldn't change directory");
-    let source_code = source_code_addition();
+    let _ = create_sandbox_and_set_as_current_directory();
     let circom_program_filename = "test.circom".to_string();
-    let mut compiler = AikenZkCompiler::from(source_code.clone());
+    let mut compiler = AikenZkCompiler::from(source_code_addition());
     compiler.save_into_file(circom_program_filename.clone()).unwrap();
 
     let res = compiler.create_verification_key(circom_program_filename);
@@ -32,8 +33,21 @@ fn test_compiler_can_compile_the_generated_circom_component(){
     assert!(res.is_ok());
     let stored_vk = fs::read_to_string("build/verification_key.json").expect("No se pudo leer el archivo");
     assert!(stored_vk.contains("protocol"));
+}
 
-    clear_sandbox();
+fn create_sandbox_and_set_as_current_directory() -> TempDir {
+    let sandbox_path = &sandbox_path();
+    let source = Path::new(sandbox_path);
+    let temp_dir = tempdir().expect("Could not create temp dir");
+    let mut options = CopyOptions::new();
+    options.copy_inside = true;
+    copy(source, temp_dir.path(), &options).expect("Could not copy template into directory");
+    env::set_current_dir(temp_dir.path()).expect("Couldn't move to temp directory");
+
+    let perms = fs::Permissions::from_mode(0o777);
+    fs::set_permissions(&temp_dir.path(), perms).expect("Failed to set permissions");
+
+    temp_dir
 }
 
 fn sandbox_path() -> String {
@@ -42,11 +56,6 @@ fn sandbox_path() -> String {
 
 fn manifest_path() -> String {
     env!("CARGO_MANIFEST_DIR").to_string()
-}
-
-fn clear_sandbox() {
-    let _ = fs::remove_file("test.circom");
-    let _ = fs::remove_dir_all("build");
 }
 
 fn source_code_addition() -> String {
