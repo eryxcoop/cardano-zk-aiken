cf25b16ff0e52127f3fdd7627e5c7c18704c5064
bruno <bweisz@eryxsoluciones.com.ar>
2025-06-18 15:29:01 -0300

    wip

--- aiken-zk/src/aiken_zk_compiler.rs
+++ aiken-zk/src/aiken_zk_compiler.rs
@@ -1,16 +1,57 @@
 use crate::circom_compiler::CircomCompiler;
 use crate::component_creator::ComponentCreator;
 use crate::lexer::{LexInfo, Lexer};
-use crate::token_zk::TokenZK as Token;
+use crate::token_zk::{TokenZK as Token, TokenZK};
 use aiken_lang::ast::Span;
+use chumsky::combinator::To;
+use crate::zk_examples::{InputVisibility, ZkExample};
 
 pub struct AikenZkCompiler;
 
 impl AikenZkCompiler {
 
 
-    pub fn find_offchain_token(tokens: Vec<(Token, Span)>) -> Some<&'static (Token, Span)> {
+    fn find_offchain_token(tokens: Vec<(Token, Span)>) -> Some<&'static (Token, Span)> {
         tokens.iter().find(|(token, span)| matches!(token, Token::Offchain {..}))
+    }
+
+    fn replace_keyword_with_function_call(aiken_src: &str, token: &Token, span: &Span) -> String {
+        let mut aiken_zk_src = String::from(aiken_src);
+        let public_identifiers = Self::extract_public_identifiers_from_token(token);
+
+        let replacement = String::new();
+        aiken_zk_src.replace_range(span.start..span.end, &replacement);
+        aiken_zk_src
+    }
+
+    fn extract_identifier_from_token(token: &Token) -> String {
+        match token {
+            Token::Name {name} => name.clone(),
+            Token::Int {value, ..} => value.clone(),
+            _ => panic!("Not the expected kind of token")
+        }
+    }
+
+    fn extract_public_identifiers_from_token(token: &TokenZK) {
+        match token {
+            Token::Offchain { example: ZkExample::Addition { lhs, rhs, res } } => {
+                let public_inputs_identifiers: Vec<String> = [lhs, rhs, res].iter().fold(
+                    vec![],
+                    |mut acc, &input| match input.visibility.clone() {
+                        Some(InputVisibility::Public) => {
+                                acc.push(Self::extract_identifier_from_token(token));
+                                acc
+                            }
+                        None => {
+                            acc.push(Self::extract_identifier_from_token(token));
+                            acc
+                        }
+                        _ => acc
+                    },
+                );
+            }
+            _ => panic!("Not implemented")
+        }
     }
 
     pub fn apply_modifications_to_src_for_token(aiken_src: String, aiken_src_filename: String) -> String {
@@ -26,6 +67,7 @@ impl AikenZkCompiler {
         // Leer vk
         // Comprimir los puntos de curva
         // crear .ak modificado con el contenido de la vk
+
 
 
         Ok("".to_string())
--- aiken-zk/src/tests/tests_aiken_zk_compiler.rs
+++ aiken-zk/src/tests/tests_aiken_zk_compiler.rs
@@ -9,6 +9,6 @@ fn test_compiler_can_replace_addition_of_public_variables_by_the_corresponding_f
     let _temp_dir = create_sandbox_and_set_as_current_directory();
     let keyword = "offchain addition(pub a, pub b, pub c)\n";
     let aiken_src = aiken_template_with_keyword(keyword);
-    let aiken_src_filename = "my_program";
+    let aiken_src_filename = "my_program".to_string();
     let aiken_zk_src = AikenZkCompiler::apply_modifications_to_src_for_token(aiken_src, aiken_src_filename);
 }
