dfa367b1defb1ed4eb8c599c5366747bc12104f6
bruno <bweisz@eryxsoluciones.com.ar>
2025-06-24 14:30:08 -0300

    test for mixed visibility addition

--- aiken-zk/src/tests/tests_aiken_zk_compiler.rs
+++ aiken-zk/src/tests/tests_aiken_zk_compiler.rs
@@ -27,16 +27,17 @@ fn test_compiler_can_replace_addition_of_private_variables_by_the_corresponding_
     );
 }
 
-/*#[test]
+#[test]
 #[serial]
 fn test_compiler_can_replace_addition_of_mixed_variables_by_the_corresponding_funcion_and_call(){
     test_compiler_can_replace_addition_by_the_corresponding_funcion_and_call(
         "offchain addition(priv a, b, pub c)",
-        "zk_verify_or_fail(redeemer, [b, c])"
+        "zk_verify_or_fail(redeemer, [b, c])",
+        addition_mixed_visibility_vk_compressed(), 2
     );
 }
 
-#[test]
+/*#[test]
 #[serial]
 fn test_compiler_can_replace_addition_of_mixed_variables_and_constants_by_the_corresponding_funcion_and_call(){
     test_compiler_can_replace_addition_by_the_corresponding_funcion_and_call(
@@ -44,7 +45,6 @@ fn test_compiler_can_replace_addition_of_mixed_variables_and_constants_by_the_co
         "zk_verify_or_fail(redeemer, [4, b])"
     );
 }*/
-
 fn test_compiler_can_replace_addition_by_the_corresponding_funcion_and_call(
     original: &str,
     replacement: &str,
@@ -95,6 +95,20 @@ fn addition_all_private_vk_compressed() -> Groth16CompressedData {
         vk_delta_2: "93e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb8".to_string(),
         IC: vec![
             "b8fcac9bb8eebddd4daf43519eb65d952436f5e98be287e246d70fc27f267379e132a156f6a4a742ece62fbb7c5e220d".to_string(),
+        ],
+    }
+}
+
+fn addition_mixed_visibility_vk_compressed() -> Groth16CompressedData {
+    Groth16CompressedData {
+        vk_alpha_1: "85e3f8a13a670514351a68677ea0e2fc51150daeea496b85a34d97751695e26b2ae4f1a5a3b60e17bb7bfd6d474154c5".to_string(),
+        vk_beta_2: "b1abf58f58af5981cd24f996e53626a4157eeed4aa814498885b3a547c35d5efb877834602508255c030708552b353e21631f16475e35b977e39a068ac9fb5bc4c25d383139b721da0a878b663c4df52c94a51f7c06a019bb40324713d2bbf0f".to_string(),
+        vk_gamma_2: "93e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb8".to_string(),
+        vk_delta_2: "93e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb8".to_string(),
+        IC: vec![
+            "af1ca9d68a382928932cd5f1a3dde62489556f42da0c24e6d11191c1b187f147a206da840166e28f1ae73edee0c8b912".to_string(),
+            "aba434215d34bebf3100b82fb68eaa69328cc6431a26ecc8ef81bffced149a5f7e193587789a1a0c6745b3e963c1989e".to_string(),
+            "87db49b3c35ae1d3f5b767abf48ca5b73d17c81ad5c50419386a09415e7eba5b7bf50e5d3d2976ec11c31ad4f2ec3477".to_string(),
         ],
     }
 }
