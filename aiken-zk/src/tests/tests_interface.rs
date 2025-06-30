987d1d9837930142eea3b21694d736400c825203
bruno <bweisz@eryxsoluciones.com.ar>
2025-06-30 12:54:47 -0300

    Cleaned sandbox

--- aiken-zk/src/tests/tests_interface.rs
+++ aiken-zk/src/tests/tests_interface.rs
@@ -1,7 +1,7 @@
 use crate::tests::utils::{create_sandbox_and_set_as_current_directory, manifest_path};
 use serial_test::serial;
 use std::fs::File;
-use std::io;
+use std::{fs, io};
 use std::io::BufRead;
 use std::path::Path;
 use std::process::Command;
@@ -12,8 +12,9 @@ fn test_user_can_convert_aiken_with_offchain_to_valid_aiken() {
     let _temporal_directory = create_sandbox_and_set_as_current_directory();
     let aiken_zk_binary_path = manifest_path() + "/target/debug/aiken-zk";
     let output_path = "validators/output.ak";
+    create_original_aiken_file_and_inputs();
     Command::new(aiken_zk_binary_path)
-        .arg("original_aiken_code.ak")
+        .arg(source_aiken_filename())
         .arg(output_path)
         .output()
         .unwrap();
@@ -34,4 +35,47 @@ fn test_user_can_convert_aiken_with_offchain_to_valid_aiken() {
     let compilation_result = Command::new("aiken").arg("build").output().unwrap();
     assert!(compilation_result.status.success());
     assert!(Path::new("plutus.json").exists());
+}
+
+fn source_aiken_filename() -> &'static str {
+    "original_aiken_code.ak"
+}
+
+fn create_original_aiken_file_and_inputs() {
+    fs::create_dir("validators").expect("Couldnt create dir");
+    fs::write(source_aiken_filename(), original_aiken_code()).expect("output file write failed");
+    // fs::write("inputs.json", inputs_json()).expect("output file write failed");
+}
+
+fn inputs_json() -> String {
+    r#"{"a": "3", "b": "7", "c": "10"}"#.to_string()
+}
+
+fn original_aiken_code() -> String{
+    r#"use cardano/transaction.{OutputReference, Transaction,}
+
+pub type ZK<redeemer_type> {
+  redeemer: redeemer_type,
+  proofs: List<Proof>,
+}
+
+type Redeemer = Void
+
+validator example {
+  spend(
+    datum: Option<Int>,
+    redeemer: ZK<Redeemer>,
+    _own_ref: OutputReference,
+    _self: Transaction,
+  ) {
+    expect Some(b) = datum
+    expect _zk_redeemer = offchain addition(priv a, b, 10)
+    True
+  }
+
+  else(_) {
+    fail
+  }
+}
+"#.to_string()
 }
