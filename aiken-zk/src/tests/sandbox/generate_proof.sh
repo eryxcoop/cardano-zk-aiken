fd68597e911eb81d7faa77f762977b8ba6cad970
Tom Grosso <tgrosso@eryx.co>
2025-07-01 15:23:32 -0300

    Generate proof script takes output_path

--- aiken-zk/src/tests/sandbox/generate_proof.sh
+++ aiken-zk/src/tests/sandbox/generate_proof.sh
@@ -8,14 +8,15 @@ VERIFICATION_KEY_PATH="$2"
 
 INPUT_PATH="$3"
 
-OUTPUT_PATH="build/"
-mkdir -p ${OUTPUT_PATH}
+OUTPUT_PATH="$4"
 
+BUILD_PATH="build/"
+mkdir -p ${BUILD_PATH}
 
-circom "${CIRCUIT_PATH}" --r1cs --wasm --sym -p bls12381 -o ${OUTPUT_PATH}
+circom "${CIRCUIT_PATH}" --r1cs --wasm --sym -p bls12381 -o ${BUILD_PATH}
 
-node "${OUTPUT_PATH}${CIRCUIT_NAME}_js/generate_witness.js" "${OUTPUT_PATH}${CIRCUIT_NAME}_js/${CIRCUIT_NAME}.wasm" "${INPUT_PATH}"  ${OUTPUT_PATH}witness.wtns
+node "${BUILD_PATH}${CIRCUIT_NAME}_js/generate_witness.js" "${BUILD_PATH}${CIRCUIT_NAME}_js/${CIRCUIT_NAME}.wasm" "${INPUT_PATH}"  ${BUILD_PATH}witness.wtns
 
-snarkjs groth16 prove "${VERIFICATION_KEY_PATH}" ${OUTPUT_PATH}witness.wtns ${OUTPUT_PATH}proof.json ${OUTPUT_PATH}public.json
+snarkjs groth16 prove "${VERIFICATION_KEY_PATH}" ${BUILD_PATH}witness.wtns ${BUILD_PATH}proof.json ${BUILD_PATH}public.json
 
-node curve_compress/compressedProof.js ${OUTPUT_PATH}proof.json > proof.ak
+node curve_compress/compressedProof.js ${BUILD_PATH}proof.json > "${OUTPUT_PATH}"
