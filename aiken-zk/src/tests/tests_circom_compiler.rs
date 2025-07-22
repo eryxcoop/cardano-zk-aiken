2530aa343cadde9bf044b8defe5dee28c73e6e38
Tom Grosso <tgrosso@eryx.co>
2025-07-22 17:00:17 -0300

    Add test for proof generation by the circom compiler

--- aiken-zk/src/tests/tests_circom_compiler.rs
+++ aiken-zk/src/tests/tests_circom_compiler.rs
@@ -2,6 +2,8 @@ use crate::circom_compiler::CircomCompiler;
 use crate::tests::utils::create_sandbox_and_set_as_current_directory;
 use serial_test::serial;
 use std::fs;
+use chumsky::primitive::Container;
+use crate::compressed_groth16_proof_bls12_381::CompressedGroth16ProofBls12_381;
 
 #[test]
 #[serial]
@@ -40,6 +42,33 @@ fn test_compiler_can_compile_the_generated_circom_component() {
     assert!(stored_vk.contains("protocol"));
 }
 
+#[test]
+#[serial]
+fn test_xxx_creates_proof_object() {
+    let _temp_dir = create_sandbox_and_set_as_current_directory();
+    let circom_program_filename = "test.circom".to_string();
+    let mut compiler = CircomCompiler::from(source_code_addition());
+    compiler
+        .save_into_file(circom_program_filename.clone())
+        .unwrap();
+    fs::write("inputs.json", "{\"a\":\"1\", \"b\":\"2\", \"c\":\"3\"}").unwrap();
+
+    let proof =
+        CircomCompiler::generate_proof("test.circom", "my_verification_key.zkey", "inputs.json");
+
+    assert_proof_is_valid(proof);
+}
+
+// Ideally we would run a verification using the proof and the circuit
+// but this would slow down noticeably the tests
+fn assert_proof_is_valid(proof: CompressedGroth16ProofBls12_381) {
+    assert_eq!(96, proof.piA_as_byte_string().len());
+    assert!(proof.piA_as_byte_string().chars().into_iter().all(|c| c.is_ascii_hexdigit()));
+    assert_eq!(192, proof.piB_as_byte_string().len());
+    assert!(proof.piB_as_byte_string().chars().into_iter().all(|c| c.is_ascii_hexdigit()));
+    assert_eq!(96, proof.piC_as_byte_string().len());
+    assert!(proof.piC_as_byte_string().chars().into_iter().all(|c| c.is_ascii_hexdigit()));
+}
 // ---------- AUX ---------- //
 
 fn source_code_addition() -> String {
