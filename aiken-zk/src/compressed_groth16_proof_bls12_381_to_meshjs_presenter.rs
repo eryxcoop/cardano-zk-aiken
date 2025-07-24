a0dd5b3c2467d19ab83cfc79bafc10ce945bca00
Tom Grosso <tgrosso@eryx.co>
2025-07-24 15:48:48 -0300

    wip -

--- aiken-zk/src/compressed_groth16_proof_bls12_381_to_meshjs_presenter.rs
+++ aiken-zk/src/compressed_groth16_proof_bls12_381_to_meshjs_presenter.rs
@@ -26,18 +26,44 @@ impl CompressedGroth16ProofBls12_381ToMeshJsPresenter {
     }
 
     pub fn present(&self, file_prefix: String) -> String {
-        let mut presented_proof = file_prefix;
+        let mut presented_proof = self.file_prefix();
         presented_proof += "\t\tmProof(\n";
         presented_proof += &("\t\t\t\"".to_string() + self.proof.pi_a_as_byte_string() + "\",\n");
         presented_proof += &("\t\t\t\"".to_string() + self.proof.pi_b_as_byte_string() + "\",\n");
         presented_proof += &("\t\t\t\"".to_string() + self.proof.pi_c_as_byte_string() + "\",\n");
         presented_proof += "\t\t),\n";
-        presented_proof += &Self::file_suffix();
+        presented_proof += &self.file_suffix();
 
         presented_proof
     }
 
-    fn file_suffix() -> String {
+    fn file_prefix(&self) -> String {
+        r#"import {MConStr} from "@meshsdk/common";
+import {Data, mConStr0} from "@meshsdk/core";
+
+type Proof = MConStr<any, string[]>;
+
+type ZKRedeemer = MConStr<any, Data[] | Proof[]>;
+
+function mProof(piA: string, piB: string, piC: string): Proof {
+    if (piA.length != 96 || piB.length != 192 || piC.length != 96) {
+        throw new Error("Wrong proof");
+    }
+
+    return mConStr0([piA, piB, piC]);
+}
+
+export function mZKRedeemer(redeemer: Data): ZKRedeemer {
+    return mConStr0([redeemer, proofs()]);
+}
+
+function proofs(): Proof[] {
+    return [
+"#
+            .to_string()
+    }
+    
+    fn file_suffix(&self) -> String {
         r#"    ];
 }
 "#.to_string()
