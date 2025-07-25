c1a8e9c3d826e12918ad62732a7b4fb213ece053
gonza-lenzi <lenzigonzalot@gmail.com>
2025-07-25 12:49:07 -0300

    wip

--- aiken-zk/src/meshjs_zk_redeemer_presenter.rs
+++ aiken-zk/src/meshjs_zk_redeemer_presenter.rs
@@ -1,31 +1,5 @@
 use crate::compressed_groth16_proof_bls12_381::CompressedGroth16ProofBls12_381;
-
-pub struct CompressedGroth16ProofBls12_381ToMeshJsPresenter {
-    proof: CompressedGroth16ProofBls12_381
-}
-
-impl CompressedGroth16ProofBls12_381ToMeshJsPresenter {
-
-    pub fn new_for(proof: CompressedGroth16ProofBls12_381) -> Self {
-        Self {
-            proof
-        }
-    }
-
-    pub fn present(&self) -> String {
-        format!(
-            "\t\tmProof(
-\t\t\t\"{}\",
-\t\t\t\"{}\",
-\t\t\t\"{}\",
-\t\t),
-",
-            self.proof.pi_a_as_byte_string(),
-            self.proof.pi_b_as_byte_string(),
-            self.proof.pi_c_as_byte_string()
-        )
-    }
-}
+use crate::compressed_groth16_proof_bls12_381_to_meshjs_presenter::CompressedGroth16ProofBls12_381ToMeshJsPresenter;
 
 pub struct MeshJsZKRedeemerPresenter {
     proof_presenter: CompressedGroth16ProofBls12_381ToMeshJsPresenter
