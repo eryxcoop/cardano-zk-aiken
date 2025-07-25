8335427f695d1d4580674dbd5cf415acdf112075
Tom Grosso <tgrosso@eryx.co>
2025-07-25 17:57:46 -0300

    wip

--- aiken-zk/src/command_line_interface.rs
+++ aiken-zk/src/command_line_interface.rs
@@ -56,7 +56,7 @@ impl BuildCommand {
 }
 
 struct ProveCommand {
-    
+
 }
 
 impl Subcommand for ProveCommand {}
@@ -142,34 +142,6 @@ impl CommandLineInterface {
             });
         } else {
         }
-    }
-
-    fn execute_prove_command(
-        circom_path: &Path,
-        verification_key_path: &Path,
-        inputs_path: &Path,
-        output_path: &Path,
-    ) {
-        AikenZkCompiler::generate_aiken_proof(
-            circom_path.to_str().unwrap(),
-            verification_key_path.to_str().unwrap(),
-            inputs_path.to_str().unwrap(),
-            output_path.to_str().unwrap(),
-        );
-    }
-
-    fn get_prove_arguments(
-        subcommand_matches: &ArgMatches,
-    ) -> (&PathBuf, &PathBuf, &PathBuf, &PathBuf) {
-        let circom_path =
-            Self::get_argument_value(subcommand_matches, Self::PROVE_COMMAND_CIRCOM_ARG_NAME);
-        let verification_key_path =
-            Self::get_argument_value(subcommand_matches, Self::PROVE_COMMAND_VK_ARG_NAME);
-        let inputs_path =
-            Self::get_argument_value(subcommand_matches, Self::PROVE_COMMAND_INPUT_ARG_NAME);
-        let output_path =
-            Self::get_argument_value(subcommand_matches, Self::PROVE_COMMAND_OUTPUT_ARG_NAME);
-        (circom_path, verification_key_path, inputs_path, output_path)
     }
 
     fn create_main_command() -> Command {
