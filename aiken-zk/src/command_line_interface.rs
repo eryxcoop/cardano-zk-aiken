d48ac871eb1f0f94d0abbc04229213512a736ff7
Tom Grosso <tgrosso@eryx.co>
2025-07-28 12:26:06 -0300

    wip

--- aiken-zk/src/command_line_interface.rs
+++ aiken-zk/src/command_line_interface.rs
@@ -3,7 +3,9 @@ use crate::prove_command::ProveCommand;
 use crate::subcommand::Subcommand;
 use clap::Command;
 
-pub struct CommandLineInterface;
+pub struct CommandLineInterface {
+    main_command: Command,
+}
 
 macro_rules! execute_subcommand {
     ( $subcommand: ident, $a_command: ident, $( $others_commands:ident ),* ) => {
@@ -27,10 +29,18 @@ macro_rules! execute_subcommand {
     };
 }
 impl CommandLineInterface {
-    pub fn parse_inputs_and_execute_command() {
-        let main_command = Self::create_main_command();
-        
-        let main_command_matches = main_command.get_matches();
+    pub fn create_main_command() -> Self {
+        let main_command = Command::new("aiken-zk")
+            .subcommand_required(true)
+            .subcommand(BuildCommand::create_subcommand())
+            .subcommand(ProveCommand::create_subcommand());
+        Self {
+            main_command
+        }
+    }
+
+    pub fn parse_inputs_and_execute_command(&self) {
+        let main_command_matches = self.main_command.clone().get_matches();
         match main_command_matches.subcommand() {
             Some(subcommand) => {
                 execute_subcommand!(subcommand, BuildCommand, ProveCommand);
@@ -39,12 +49,5 @@ impl CommandLineInterface {
                 panic!("No command given");
             }
         }
-    }
-
-    fn create_main_command() -> Command {
-        Command::new("aiken-zk")
-            .subcommand_required(true)
-            .subcommand(BuildCommand::create_subcommand())
-            .subcommand(ProveCommand::create_subcommand())
     }
 }
--- aiken-zk/src/main.rs
+++ aiken-zk/src/main.rs
@@ -1,5 +1,6 @@
 use aiken_zk::command_line_interface::CommandLineInterface;
 
 fn main() {
-    CommandLineInterface::parse_inputs_and_execute_command();
+    let main_command = CommandLineInterface::create_main_command();
+    main_command.parse_inputs_and_execute_command();
 }
