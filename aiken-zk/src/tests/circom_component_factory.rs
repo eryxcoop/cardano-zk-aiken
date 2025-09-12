90aca0100be830d1c63493c718f1562cda5ef37a
brweisz <brweisz@eryx.co>
2025-09-12 11:49:12 -0300

    rename

--- aiken-zk/src/tests/circom_component_factory.rs
+++ aiken-zk/src/tests/circom_component_factory.rs
@@ -18,13 +18,13 @@ pub fn indexing_custom_circom_template_and_component() -> String {
     r#"
 pragma circom 2.1.9;
 
-template Addition() {
+template Indexing() {
     signal input l[2];
     signal input val;
     l[0] === val;
 }
 
-component main {public [l, val]} = Addition();
+component main {public [l, val]} = Indexing();
 "#
     .to_string()
 }
