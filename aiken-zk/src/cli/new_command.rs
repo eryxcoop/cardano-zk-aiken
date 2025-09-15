4fc6b275cd763af09f4f988569d5204651b5b87a
brweisz <brweisz@eryx.co>
2025-09-15 11:09:03 -0300

    WIP: Copies whole folder structure to the new dir

--- aiken-zk/src/cli/new_command.rs
+++ aiken-zk/src/cli/new_command.rs
@@ -1,8 +1,12 @@
 use crate::cli::subcommand::Subcommand;
 use clap::{ArgMatches, Command};
 use std::fs;
-use std::io::Error;
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
+use rust_embed::RustEmbed;
+
+#[derive(RustEmbed)]
+#[folder = "milestone_example/"]
+struct ProjectTemplate;
 
 pub struct NewCommand;
 
@@ -31,16 +35,33 @@ impl NewCommand {
     fn execute_command(project_name: &PathBuf) {
 
         // Create a directory called [project_name]
+        // -------------------------------------------------
         // Copy the static template tree structure in the directory
         // Run [npm i | yarn i | ...] over the desired subdirectories
         // Check for the global installation of aiken, circom, snarkjs. Warn the user if they're not installed
 
-        fs::create_dir(&project_name)
-            .expect("Unable to create working directory");
+        fs::create_dir(&project_name).expect("Unable to create working directory");
+        Self::copy_embedded_dir(project_name).expect("Failed to populate working directory");
 
     }
 
     fn get_arguments(subcommand_matches: &ArgMatches) -> &PathBuf {
         Self::get_argument_value(subcommand_matches, Self::NEW_COMMAND_PROJECT_NAME_ARG_NAME)
+    }
+
+    fn copy_embedded_dir(root: &Path) -> Result<(), std::io::Error> {
+        for file in ProjectTemplate::iter() {
+            let file_path = Path::new(file.as_ref());
+            let out_path = root.join(file_path);
+
+            if let Some(parent) = out_path.parent() {
+                fs::create_dir_all(parent)?;
+            }
+
+            let content = ProjectTemplate::get(file.as_ref()).expect("Embedded file missing");
+            fs::write(&out_path, &content.data)?;
+        }
+
+        Ok(())
     }
 }
