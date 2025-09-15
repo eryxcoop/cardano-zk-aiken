a85acf14d94cf0fb63ad2fd6ce49dfa200a05f7b
brweisz <brweisz@eryx.co>
2025-09-15 10:31:01 -0300

    WIP: Started creating "new" command

--- aiken-zk/src/cli/mod.rs
+++ aiken-zk/src/cli/mod.rs
@@ -1,4 +1,5 @@
-mod build_command;
 pub mod command_line_interface;
+mod build_command;
 mod prove_command;
+mod new_command;
 mod subcommand;
---
+++ aiken-zk/src/cli/new_command.rs
@@ -0,0 +1,55 @@
+use std::fs;
+use std::io::{Error, ErrorKind};
+use std::path::PathBuf;
+use clap::{ArgMatches, Command};
+use crate::cli::build_command::BuildCommand;
+use crate::cli::subcommand::Subcommand;
+use crate::compiler::aiken_zk_compiler::AikenZkCompiler;
+use crate::entropy_generator::EntropyGenerator;
+
+pub struct NewCommand;
+
+impl Subcommand for NewCommand {
+    const SUBCOMMAND_NAME: &'static str = "new";
+
+    fn create_subcommand() -> Command {
+        let project_name = Self::create_required_argument_with_id(Self::NEW_COMMAND_PROJECT_NAME_ARG_NAME);
+
+        Command::new(Self::SUBCOMMAND_NAME).arg(project_name.clone())
+    }
+
+    fn for_name(name: &str) -> bool {
+        name.to_string() == Self::SUBCOMMAND_NAME.to_string()
+    }
+
+    fn evaluate(&self, matches: &ArgMatches) {
+        let project_name = Self::get_arguments(matches);
+        Self::execute_command(project_name);
+    }
+}
+
+impl NewCommand {
+    const NEW_COMMAND_PROJECT_NAME_ARG_NAME: &'static str = "project_name";
+
+    fn execute_command(project_name: &PathBuf) {
+
+        // Create a directory called [project_name]
+        // Copy the static template tree structure in the directory
+        // Run [npm i | yarn i | ...] over the desired subdirectories
+        // Check for the global installation of aiken, circom, snarkjs. Warn the user if they're not installed
+
+
+        let new_dir_name = fs::read_to_string(project_name).unwrap();
+        Self::create_new_dir_if_non_existent(&new_dir_name)
+            .expect("Unable to create working directory");
+
+    }
+
+    fn get_arguments(subcommand_matches: &ArgMatches) -> &PathBuf {
+        Self::get_argument_value(subcommand_matches, Self::NEW_COMMAND_PROJECT_NAME_ARG_NAME)
+    }
+
+    pub fn create_new_dir_if_non_existent(dir_name: &str) -> Result<(), Error>{
+        fs::create_dir(dir_name)
+    }
+}
